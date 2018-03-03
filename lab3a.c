adedad23faa81bc7f80cc048b220251d6113096a
katiecai <katiecai@g.ucla.edu>
2018-03-03 15:39:11 -0800

    added free blocks

--- lab3a.c
+++ lab3a.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <stdint.h>
 #include "ext2_fs.h"
 
 #define BUFF_SIZE 2048
@@ -35,7 +36,7 @@ void superblock_summary(void)
   inode_count = superblock_ptr->s_inodes_count;
   //block size
   printf("%d,", 1024 << superblock_ptr->s_log_block_size);
-  block_size = superblock_ptr->s_log_block_size;
+  block_size = 1024 << superblock_ptr->s_log_block_size;
   //inode size
   printf("%d,", superblock_ptr->s_inode_size);
   //blocks per group
@@ -76,6 +77,33 @@ void group_summary(void)
   printf("%d\n", group_pointer->bg_inode_table);
 }
 
+void free_blocks(void) 
+{
+  uint8_t* bitmap = malloc(block_size*sizeof(uint8_t));
+  int toRead = pread(ext2fd, bitmap, block_size, blocks_bitmap*block_size);
+  if (toRead < 0)
+    {
+      // syscall error
+    }
+
+  uint32_t i;
+  uint32_t bitmap_size = block_count;
+  int counter = 0;
+
+  for (i = 0; i < bitmap_size; i++)
+    {
+      uint8_t a_byte = bitmap[i/8];
+      if (!(a_byte & (1 << (i % 8))))
+	{
+	  counter++;
+	  printf("BFREE,");
+	  printf("%d\n", i);
+	}
+    }
+
+  printf("%d\n", counter);
+}
+
 void free_inodes(void)
 {
   
@@ -97,5 +125,6 @@ int main(int argc, char* argv[])
     }
   superblock_summary();
   group_summary();
+  free_blocks();
   return 0;
 }
